9e40536e23f3ec91060b15fff3915ce7bcae891c
admin <gbtunney@mac.com>
2017-10-22 22:59:52 -0400

    [VP] Rollback to the same state as of 22-Oct-17 22:46:49
    VP-Action: versionpress/rollback/4ce0d5e01798025adb3ea40a39b2124eb81f425b
    
    X-VP-Version: 4.0-alpha1
    X-VP-Environment: dev

--- wp-content/themes/Avada-Child-Theme/gulpfile.js
+++ wp-content/themes/Avada-Child-Theme/gulpfile.js
@@ -145,7 +145,7 @@ gulp.task( 'browser-sync', function() {
 
     // Inject CSS changes.
     // Commnet it to reload browser for every CSS change.
-    injectChanges: false,
+    injectChanges: true,
 
     // Use a specific port (instead of the one auto-detected by Browsersync).
     //port: 7000,
